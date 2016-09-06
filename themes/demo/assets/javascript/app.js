6c35c31fd0720bfe3daf27ccde21077bcecb86ae
johnnywo <office@zeero.at>
2016-09-06 17:14:57 +0200

    plugin martin bloggallery etc 2

--- themes/demo/assets/javascript/app.js
+++ themes/demo/assets/javascript/app.js
@@ -6,3 +6,13 @@ $(document).tooltip({
     selector: "[data-toggle=tooltip]"
 })
 
+$(document).ready(function() {
+    $('.sidebar-navbar-collapse').on('click','.list-group-item', function ( e ) {
+        e.preventDefault();
+        $(this).parents('.sidebar-navbar-collapse').find('.active').removeClass('active').end().end().addClass('active');
+        $(activeTab).show();
+    });
+
+
+});
+
